176efdb9f9ff82de90e8a45dc3bf6803f9682913
Thái meo <thaiphph52804@gmail.com>
2024-11-04 10:22:04 +0700

    vueBt

--- src/components/StateVueJs.vue
+++ src/components/StateVueJs.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>Count:{{ count }}</div>
   <button @click="tang">Tăng</button>
 </template>
@@ -8,6 +8,21 @@ import { ref } from 'vue'
 const count = ref(0)
 const tang = () => {
   count.value++
+}
+</script>
+
+<style lang="scss" scoped></style> -->
+
+<template>
+  <div>Notification: {{ notification }}</div>
+  <button @click="change">Change</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const notification = ref('offline')
+const change = () => {
+  notification.value = 'online'
 }
 </script>
 
