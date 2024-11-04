1839767b981b23461ac4cfd829eb0f8ecdca81ca
Thái meo <thaiphph52804@gmail.com>
2024-11-04 09:47:23 +0700

    Vue

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import StateVueJs from './components/StateVueJs.vue'
+</script>
+
+<template>
+  <StateVueJs />
+</template>
+
+<style scoped></style>
---
+++ src/components/StateVueJs.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Count:{{ count }}</div>
+</template>
+
+<script setup>
+const count = 0
+</script>
+
+<style lang="scss" scoped></style>
